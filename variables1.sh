77530e12413b5ce9cf6639876980bb99313611b4
Srinivasulu Arikatla Mohan Rao <shree@jaishreeram.hanuma>
2024-05-30 21:17:25 +0100

    added var

--- variables1.sh
+++ variables1.sh
@@ -3,7 +3,7 @@
 x=Sachin 
 y=Rohit
 
-echo "x : Hi y good morning"
-echo "y : Hello x Am great how you doing?"
-echo "x : Am doing great too. Are you heading towards work y?" 
-echo "y : Yes I can drop you on the way x"
+echo "$x : Hi $y good morning"
+echo "$y : Hello $x Am great how you doing?"
+echo "$x : Am doing great too. Are you heading towards work $y?" 
+echo "$y : Yes I can drop you on the way $x"
