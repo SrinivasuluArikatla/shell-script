793ca8b9b8b41d697d237da6cd72f44a2a041851
Srinivasulu Arikatla Mohan Rao <shree@jaishreeram.hanuma>
2024-05-30 21:11:26 +0100

    added var

---
+++ variables1.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+x=Sachin 
+y=Rohit
+
+echo "x : Hi y good morning"
+echo "y : Hello x Am great how you doing?"
+echo "x : Am doing great too. Are you heading towards work y?" 
+echo "y : Yes I can drop you on the way x"
